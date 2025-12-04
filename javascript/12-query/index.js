88b7983d8620045492b5f383f8b6002ad93692f3
Ilya Shadrin <inshildr@gmail.com>
2025-12-04 13:44:35 +0300

    13-query.v2

--- javascript/12-query/index.js
+++ javascript/12-query/index.js
@@ -6,14 +6,11 @@ const user = {
     location: 'moscow'
 }
 
-//Первый вариант
-const queryString_ver1 = new URLSearchParams(user).toString();
+console.log(toQueryString(use));
 
-console.log(queryString_ver1);
-
-//Второй вариант
-const queryString_ver2 = Object.entries(user)
-    .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
-    .join('&');
-
-console.log(queryString_ver2);
+function toQueryString(){
+    return Object.entries(params)
+        .filter(([_, val]) => val !== undefined && val !== null)
+        .map(([key, val]) => `${encodeURIComponent(key)}=${encodeURIComponent(val)}`)
+        .join('&');
+}
