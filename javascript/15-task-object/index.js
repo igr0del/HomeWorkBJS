c44c0749f3b9e95e2ee5deaa20af266d38c60d44
Shadrin Ilya <inshildr@gmail.com>
2025-12-07 18:47:22 +0300

    13-task-object4

--- javascript/15-task-object/index.js
+++ javascript/15-task-object/index.js
@@ -1,48 +1,60 @@
-const ToDolist = {
-	tasks: [],
+const ToDoList = {
+  tasks: [],
 
-	addTask(title, priority) {
-		if (!title || !priority) {
-			throw new Error('Title and priority are required to add a task.');
-		}
+  // Добавить задачу
+  addTask(title, priority) {
+    if (!title) {
+      throw new Error('Title is required to add a task.');
+    }
 
-		const id = Date.now().toString();
-		const newTask = {
-			title,
-			id,
-			priority
-		}
-		
-		this.tasks.push(newTask);
-		return task;
-	},
+    if (priority == null) { // позволяет использовать 0, если вдруг захочешь
+      throw new Error('Priority is required to add a task.');
+    }
 
-	deleteTask(id) {
-		this.tasks = this.tasks.filter(task => task.id !== id);
-	}, 
+    const id = Date.now().toString();
+    const newTask = { title, id, priority };
 
-	updateTask(id, updatedProperties) {
-		const task = this.tasks.find(task => task.id === id);
-		if (!task) {
-			throw new Error('Task not found.');
-		}
+    this.tasks.push(newTask);
+    return newTask; // было return task (переменной нет)
+  },
 
-		if (updatedProperties.title !== undefined) {
-			task.title = updatedProperties.title;
-		}
+  // Удалить задачу по id
+  deleteTask(id) {
+    this.tasks = this.tasks.filter(task => task.id !== id);
+  },
 
-		if (updatedProperties.priority !== undefined) {
-			task.priority = updatedProperties.priority;
-		}
+  // Синоним, чтобы твой пример внизу тоже работал
+  removeTask(id) {
+    this.deleteTask(id);
+  },
 
-		return true;
-	},
+  // Обновить имя или приоритет по id
+  updateTask(id, updatedProperties = {}) {
+    const task = this.tasks.find(task => task.id === id);
+    if (!task) {
+      throw new Error('Task not found.');
+    }
 
-	sortByPriority() {
-		this.tasks.sort((a, b) => a.priority - b.priority);
-	}
+    const { title, priority } = updatedProperties;
+
+    if (title !== undefined) {
+      task.title = title;
+    }
+
+    if (priority !== undefined) {
+      task.priority = priority;
+    }
+
+    return task; // полезнее вернуть обновлённую задачу, а не true
+  },
+
+  // Отсортировать по приоритету
+  sortByPriority() {
+    this.tasks.sort((a, b) => a.priority - b.priority);
+  }
 };
 
+// Пример использования
 ToDoList.addTask("Помыть посуду", 1);
 ToDoList.addTask("Вынести мусор", 3);
 ToDoList.addTask("Сделать уроки", 2);
@@ -53,7 +65,6 @@ ToDoList.sortByPriority();
 console.log("После сортировки:", ToDoList.tasks);
 
 ToDoList.updateTask(ToDoList.tasks[0].id, { priority: 5 });
-
 console.log("После обновления:", ToDoList.tasks);
 
 ToDoList.removeTask(ToDoList.tasks[0].id);
